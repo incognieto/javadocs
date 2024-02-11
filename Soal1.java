65f4a1a7a1132cdae158da990d8710235a0bd545
nieto <82154376+incognieto@users.noreply.github.com>
2024-02-12 06:26:00 +0700

    Add files pertemuan 1

---
+++ Soal1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author nietoLokal
+ */
+public class Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        byte angka1 = 100;
+        byte angka2 = 45;
+        byte hasil = (byte) (angka1+angka2);
+        
+        System.out.println("Hasil 1 "+hasil);
+    }
+}
